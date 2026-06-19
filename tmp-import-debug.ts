1e956602742a17f45f00e7a1882aeea6f39c66be
edelle-del <egmlumabi@mymail.mapua.edu.ph>
2026-06-20 07:06:02 +0800

    deal card fixes

--- tmp-import-debug.ts
+++
@@ -1,7 +0,0 @@
-void (async () => {
-  const m = await import('./src/lib/auth-engine.ts');
-  console.log('module keys', Object.keys(m));
-  console.log('authorizeRequest is', typeof m.authorizeRequest);
-  console.log('default is', typeof m.default);
-  console.log('default keys', m.default ? Object.keys(m.default) : null);
-})();
--- tmp-import-test.ts
+++
@@ -1,7 +0,0 @@
-(async () => {
-  const m = await import('./src/lib/auth-engine.ts');
-  console.log('module keys', Object.keys(m));
-  console.log('authorizeRequest', m.authorizeRequest);
-  console.log('default keys', m.default && Object.keys(m.default));
-  console.log('default authorizeRequest', m.default?.authorizeRequest);
-})();
